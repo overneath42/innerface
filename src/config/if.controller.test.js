15793b6e199f93b88a423ed110e9958530998092
Justin Toon <jtoon@missiondata.com>
2017-06-01 22:09:37 -0400

    Scaffold out Controller tests

---
+++ src/config/if.controller.test.js
@@ -0,0 +1,21 @@
+import Controller from './if.controller';
+
+describe('Controller', () => {
+  beforeEach(() => {
+
+  });
+
+  describe('getTargets', () => {
+    test('can convert all selector strings to element selections', () => {
+      // YAY!
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('initEventListeners', () => {
+
+  });
+});
+
+
+ 
