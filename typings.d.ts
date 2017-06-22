1def267b4336bbb5ff96564ff5800af787c7249c
Justin Toon <jtoon@missiondata.com>
2017-06-21 22:50:20 -0400

    Fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,22 +1,32 @@
-// Type definitions for Innerface 0.1.0
-// Project: https://github.com/overneath42/innerface
-// Definitions by: Justin Toon <http://justintoon.com>
-// TypeScript Version: 2.3
+// Type definitions for Innerface 0.1.0 Project:
+// https://github.com/overneath42/innerface Definitions by: Justin Toon
+// <http://justintoon.com> TypeScript Version: 2.3
 
 interface ConfigObject {
-  [key: string]: string
+  [key : string] : string
 }
 
 interface MethodObject {
-  [key: string]: Function
+  [key : string] : Function
 }
 
 interface KeyValueObject {
-  [key: string]: {
-    [key: string]: string
+  [key : string] : {
+    [key : string]: string
   }
 }
 
 interface NodeListObject {
-  [key: string]: NodeListOf<HTMLElement>
+  [key : string] : NodeListOf <HTMLElement | Element>
+}
+
+declare namespace Innerface {
+  interface Controller {
+    name : string;
+    targets : NodeListObject;
+    events : MethodObject;
+    methods?: MethodObject;
+    initialize(): void;
+    getTargets(name: string, targets?: string | ConfigObject): NodeListObject;
+  }
 }
