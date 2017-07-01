c8908c205e4eac62b1992ecfd8c3987bc7e8eb5b
Justin Toon <jtoon@missiondata.com>
2017-07-01 19:21:36 -0400

    Expand utilities

--- src/if.utils.ts
+++ src/if.utils.ts
@@ -3,7 +3,13 @@
  *
  * @author Justin Toon
  * @version 0.1.0
+ *
+ * @requires NPM:lodash
+ * @requires NPM:inflection
  */
+
+import * as _ from 'lodash';
+import * as inflection from 'inflection';
 
 /**
  * Evaluates a string and breaks it into component parts,
@@ -13,9 +19,9 @@
  *
  * @returns {array} An array of strings extracted from the original string.
  */
-export function decodeString(name: string): string[] {
+export function decodeString(name : string) : string[] {
   const regex = /(\w+)/g;
-  let params: string[] = [];
+  let params : string[] = [];
   let match;
 
   // loop through all RegEx matches and push results to an array
@@ -33,11 +39,12 @@ export function decodeString(name: string): string[] {
  *
  * @return {Boolean}
  */
-export function isNumber(value: string | number): boolean {
-  return !isNaN(typeof value === 'number' ? value : parseInt(value, 10));
-  // NOTE: here is an alternate version to review more closely
-  // credit to `therealbenwiley`
-  // return (value && !isNaN(value)) || value === 0;
+export function isNumber(value : string | number) : boolean {
+  return !isNaN(typeof value === 'number'
+    ? value
+    : parseInt(value, 10));
+  // NOTE: here is an alternate version to review more closely credit to
+  // `therealbenwiley` return (value && !isNaN(value)) || value === 0;
 }
 
 /**
@@ -50,15 +57,56 @@ export function isNumber(value: string | number): boolean {
  *
  * @returns {HTMLElement}
  */
-export function findParentTag(node: any, tag: string): HTMLElement | void {
-  if (node.parentNode) {
+export function findParentTag(node : any, tag : string) : HTMLElement | void {
+  if(node.parentNode) {
     while (node.parentNode) {
       node = node.parentNode;
 
-      if (node.tagName && (node.tagName === tag.toLowerCase())) return node;
+      if (node.tagName && (node.tagName === tag.toLowerCase()))
+        return node;
+      }
     }
-  }
 
   // the requested tag is not a parent of the original Node
   return undefined;
+}
+
+/**
+ * Generate a selector string to locate a hidden field for
+ * a React component to access. Accepts an attribute value in camelCase
+ * or snake-case and transforms appropriately. If the value starts with `^`,
+ * the selector will be tailored to look for all elements with the given
+ * data attribute and a value which starts with the provided selector string.
+ *
+ * @example
+ * // returns '[data-react-output="testSelectorString"]'
+ * createDataFieldSelector('testSelectorString');
+ *
+ * @example
+ * // returns '[data-ui-verify-target="testSelectorString"]`
+ * createDataFieldSelector('testSelectorString', 'dataUiVerifyTarget');
+ *
+ * @example
+ * // returns '[data-ui-verify-target^="testSelectorString"]`
+ * createDataFieldSelector('^testSelectorString', 'dataUiVerifyTarget');
+ *
+ * @export
+ * @param {string} selectorString  - The string to transform.
+ * @param {string} dataAttr  - The data attribute to apply.
+ *
+ * @return {string} - The formatted selector string.
+ */
+export function createDataFieldSelector(selectorString : string, dataAttr : string) : string {
+  let operator = '=';
+
+  if (dataAttr.indexOf('-') === -1) {
+    dataAttr = inflection.transform(dataAttr, ['underscore', 'dasherize']);
+  }
+
+  if (_.startsWith(selectorString, '^')) {
+    operator = `${selectorString.slice(0, 1)}${operator}`;
+    selectorString = selectorString.slice(1);
+  }
+
+  return `[${dataAttr}${operator}"${selectorString}"]`;
 }
